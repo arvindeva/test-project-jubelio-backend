f675f161efa94a44762849840b3a26ed0c9ea1f1
arvindeva <arvindeva@gmail.com>
2019-02-15 17:45:22 +0700

    fixed start script

--- src/knex.js
+++ src/knex.js
@@ -1,24 +1,6 @@
 // knex config
 
-module.exports =  require('knex')({
-  production: {
-    client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      tableName: 'knex_migrations'
-    },
-    seeds: {
-      tableName: './seeds'
-    }
-  },
-  development: {
-    client: 'pg',
-    connection: {
-      host: 'localhost',
-      port: 5432,
-      user: 'arvin',
-      database: 'jubelio-db',
-      charset: 'utf8'
-    }
-  }
+module.exports = require('knex')({
+  client: 'pg',
+  connection: process.env.DATABASE_URL
 });
