01d7208e5d06e16e701353fcca7da866ed27f8fe
arvindeva <arvindeva@gmail.com>
2019-02-15 20:16:27 +0700

    .env updated

--- src/fileUpload.js
+++ src/fileUpload.js
@@ -6,8 +6,8 @@ require('dotenv').config();
 
 // Setting aws configurations
 aws.config.update({
-  accessKeyId: 'AKIAJVJ5HU6IJDL6SRCQ',
-  secretAccessKey: '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',
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY,
   region: 'us-east-1'
 });
 // Decent bucket name
